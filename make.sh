bbe4809235bf9a83872df938d3aa472f1f140be4
Priyanka Ganesan <priyanka.ganesan@gatech.edu>
2015-10-29 14:05:56 -0400

    Thrift make script

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@ g++ -Wall -I/usr/local/include/thrift -c Proxyserver.cpp -o proxyserver.o
 g++ -Wall -I/usr/local/include/thrift -c Proxyserver_server.cpp -o server.o
 g++ -Wall -I/usr/local/include/thrift -c proxyserver_constants.cpp -o constants.o
 g++ -Wall -I/usr/local/include/thrift -c proxyserver_types.cpp -o types.o
-g++ -Wall -I/usr/local/include/thrift -c Something_client.cpp -o client.o
-g++ -L/usr/local/lib server.o something.o constants.o types.o -o Proxyserver_server -lthrift
-g++ -L/usr/local/lib client.o something.o constants.o types.o -o Proxyserver_client -lthrift
+g++ -Wall -I/usr/local/include/thrift -c Proxyserver_client.cpp -o client.o
+g++ -L/usr/local/lib server.o proxyserver.o constants.o types.o -o Proxyserver_server -lthrift
+g++ -L/usr/local/lib client.o proxyserver.o constants.o types.o -o Proxyserver_client -lthrift
