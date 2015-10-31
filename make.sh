65fac31c456419774d09495217ddc55b590aaec6
Priyanka Ganesan <priyanka.ganesan@gatech.edu>
2015-10-31 17:17:51 -0400

    Removing tutorial code

--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-g++ -Wall -I/usr/local/include/thrift -c Proxyserver.cpp -o proxyserver.o
-g++ -Wall -I/usr/local/include/thrift -c Proxyserver_server.cpp -o server.o
-g++ -Wall -I/usr/local/include/thrift -c proxyserver_constants.cpp -o constants.o
-g++ -Wall -I/usr/local/include/thrift -c proxyserver_types.cpp -o types.o
-g++ -Wall -I/usr/local/include/thrift -c Proxyserver_client.cpp -o client.o
-g++ -L/usr/local/lib server.o proxyserver.o constants.o types.o -o Proxyserver_server -lthrift
-g++ -L/usr/local/lib client.o proxyserver.o constants.o types.o -o Proxyserver_client -lthrift
